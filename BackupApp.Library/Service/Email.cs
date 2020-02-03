d419e14c806b6dcd0fa5508873b617372b5c637f
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-02-03 12:05:16 +0100

    first commit

---
+++ BackupApp.Library/Service/Email.cs
@@ -0,0 +1,46 @@
+﻿using BackupApp.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupApp.Library.Service
+{
+    public class Email : INotification
+    {
+        private EmailModel _model;
+
+        public Email(EmailModel model)
+        {
+            _model = model;
+        }
+
+        public void Send()
+        {
+            try
+            {
+                //EmailModel model = (EmailModel)(object)t;
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient(_model.Host);
+
+                mail.From = new MailAddress(_model.EmailFrom, _model.DisplayName);
+                mail.To.Add(_model.EmailTo);
+                mail.Subject = _model.Subject;
+                mail.Body = _model.Body;
+
+                SmtpServer.Port = _model.Port;
+                SmtpServer.Credentials = new NetworkCredential(_model.Username, _model.Password);
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ BackupApp.UI/DisplayInfo.cs
@@ -0,0 +1,92 @@
+﻿using BackupApp.Library;
+using BackupApp.Library.Models;
+using BackupApp.Library.Service;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackupApp.UI
+{
+    public partial class DisplayInfo : Form
+    {
+        private string _currentDirectory = Application.StartupPath;
+        private readonly Dictionary<BackupIconType, string> _backupIcon;
+
+        public DisplayInfo()
+        {
+            InitializeComponent();
+
+            _backupIcon = new Dictionary<BackupIconType, string>();
+            this._backupIcon.Add(BackupIconType.PROCESSING, $"{_currentDirectory}\\BackupBlue.ico");
+            this._backupIcon.Add(BackupIconType.SUCCESSFUL, $"{_currentDirectory}\\BackupGreen.ico");
+            this._backupIcon.Add(BackupIconType.ERROR, $"{_currentDirectory}\\BackupRed.ico");
+
+            this.notifyIcon.Icon = new Icon(_backupIcon[BackupIconType.PROCESSING]);
+            this.notifyIcon.Text = "Backup working...";
+
+            this.notifyIcon.Visible = true;
+            this.Load += DisplayInfo_Load;
+        }
+
+        private void DisplayInfo_Load(object sender, EventArgs e)
+        {
+            string filePath = $"{_currentDirectory}\\{GlobalConfig.BackupAppFileInfo}";
+            BackupModel backupModel = filePath.LoadFile().ConvertToBackUpModel();
+
+            Backup backup = new Backup(new SharpCompression());
+            bool status = backup.Create(_currentDirectory, backupModel);
+
+            //this.notifyIcon.Icon = (status == true) ?
+            //    new Icon(_backupIcon[BackupIconType.SUCCESSFUL]) :
+            //    new Icon(_backupIcon[BackupIconType.ERROR]);
+
+            //this.notifyIcon.Text = (status == true) ? 
+            //    $"Backup OK - {DateTime.Now}" : 
+            //    $"Backup Error - {DateTime.Now}";
+            string messageContent = "";
+
+            if (status == true)
+            {
+                this.notifyIcon.Icon = new Icon(_backupIcon[BackupIconType.SUCCESSFUL]);
+                this.notifyIcon.Text = $"Backup OK - {DateTime.Now}";
+                messageContent = $"Backup successful - Date: {DateTime.Now}";
+            }
+            else
+            {
+                this.notifyIcon.Icon = new Icon(_backupIcon[BackupIconType.ERROR]);
+                this.notifyIcon.Text = $"Backup Error - {DateTime.Now}";
+                messageContent = $"Backup unsuccessful - Date: {DateTime.Now}";
+            }
+
+            string emailConfigFilePath = $"{_currentDirectory}\\{GlobalConfig.EmailConfig}";
+            string notificationFilePath = $"{_currentDirectory}\\{GlobalConfig.BackupAppNotifFileInfo}";
+
+            Notification notification = new Notification();
+            
+            if (File.Exists(emailConfigFilePath))
+            {
+                EmailModel emailModel = emailConfigFilePath.LoadFile().ConvertToEmailModel();
+                emailModel.Body = messageContent;
+                notification.Add(new Email(emailModel));
+            }
+
+            if (File.Exists(emailConfigFilePath))
+            {
+                notification.Add(new Text(messageContent, notificationFilePath));
+            }
+
+            notification.Send();
+
+            Thread.Sleep(60*1000);
+            this.Close();
+        }
+    }
+}
